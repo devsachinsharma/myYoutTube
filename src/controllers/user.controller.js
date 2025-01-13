0827e6f1bf4edd1fe47935e9c357a29b5b5a157e
Sachin Sharma <sachin2929sharma@gmail.com>
2025-01-13 16:52:27 +0530

    Access token generation added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefereshTokens = async(userId) => {
     try {
@@ -139,4 +140,49 @@ const logoutUser = asyncHandler(async(req, res) => {
     )
 
 });
-export {registerUser, loginUser, logoutUser};
+
+const refreshAccessToken = asyncHandler(async () => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    
+    if(!incomingRefreshToken){
+        new ApiError(401, "Unauthorized Request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id);
+        if(!user){
+            new ApiError(401, "Invalid refresh token");
+        }
+    
+        if(incomingRefreshToken !== user?.refreshToken){
+            new ApiError(401, "Refresh token is expired or used");
+        }
+    
+        const { refreshToken, accessToken} = await generateAccessAndRefereshTokens(user._id);
+        
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken},
+                "Access token refreshed"
+            )
+        );
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token");
+    }
+});
+
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken};
